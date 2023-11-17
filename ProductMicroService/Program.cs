f4c15318976b014eca2168b00b0c1f957cc697bf
Shakeeb <shakeebparavannur@gmail.com>
2023-11-17 23:49:50 +0530

    Added:EntityConfiguration

--- ProductMicroService/Program.cs
+++ ProductMicroService/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ProductMicroService.DBContext;
+
 namespace ProductMicroService
 {
     public class Program
@@ -12,6 +15,7 @@ namespace ProductMicroService
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<ProductContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
